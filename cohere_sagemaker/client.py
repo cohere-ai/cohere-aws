0482cced4fb1402ca30d81ec44e311ed86742047
alex-matton <alexandre@cohere.com>
2023-04-06 02:23:50 +0000

    move model after finetune

--- cohere_sagemaker/client.py
+++ cohere_sagemaker/client.py
@@ -11,19 +11,15 @@ from botocore.exceptions import ClientError, EndpointConnectionError
 
 from cohere_sagemaker.embeddings import Embeddings
 from cohere_sagemaker.error import CohereError
-from cohere_sagemaker.generation import (Generation, Generations,
-                                         TokenLikelihood)
+from cohere_sagemaker.generation import Generation, Generations, TokenLikelihood
 from cohere_sagemaker.rerank import Reranking
 
 
 class Client:
-
     def __init__(self, endpoint_name: str, region_name: Optional[str] = None, output_model_dir: Optional[str] = None):
         self._endpoint_name = endpoint_name
         self._region_name = region_name
-        self._client = boto3.client(
-            'sagemaker-runtime',
-            region_name=region_name)
+        self._client = boto3.client("sagemaker-runtime", region_name=region_name)
         self._output_model_dir = output_model_dir  # Only used for finetuning
 
         if endpoint_name == "cohere-classification-finetuning":
@@ -47,20 +43,20 @@ class Client:
         stop_sequences: List[str] = None,
         return_likelihoods: str = None,
         truncate: str = None,
-        variant: str = None
+        variant: str = None,
     ) -> Generations:
 
         json_params = {
-            'prompt': prompt,
-            'max_tokens': max_tokens,
-            'temperature': temperature,
-            'k': k,
-            'p': p,
-            'frequency_penalty': frequency_penalty,
-            'presence_penalty': presence_penalty,
-            'stop_sequences': stop_sequences,
-            'return_likelihoods': return_likelihoods,
-            'truncate': truncate
+            "prompt": prompt,
+            "max_tokens": max_tokens,
+            "temperature": temperature,
+            "k": k,
+            "p": p,
+            "frequency_penalty": frequency_penalty,
+            "presence_penalty": presence_penalty,
+            "stop_sequences": stop_sequences,
+            "return_likelihoods": return_likelihoods,
+            "truncate": truncate,
         }
         for key, value in list(json_params.items()):
             if value is None:
@@ -68,75 +64,65 @@ class Client:
         json_body = json.dumps(json_params)
 
         params = {
-            'EndpointName': self._endpoint_name,
-            'ContentType': 'application/json',
-            'Body': json_body,
+            "EndpointName": self._endpoint_name,
+            "ContentType": "application/json",
+            "Body": json_body,
         }
         if variant is not None:
-            params['TargetVariant'] = variant
+            params["TargetVariant"] = variant
 
         try:
             result = self._client.invoke_endpoint(**params)
-            response = json.loads(result['Body'].read().decode())
+            response = json.loads(result["Body"].read().decode())
         except EndpointConnectionError as e:
             raise CohereError(e)
         except Exception as e:
             # TODO should be client error - distinct type from CohereError?
-            # ValidationError, e.g. when variant is bad 
+            # ValidationError, e.g. when variant is bad
             raise CohereError(e)
 
         generations: List[Generation] = []
-        for gen in response['generations']:
+        for gen in response["generations"]:
             token_likelihoods = None
-                
-            if 'token_likelihoods' in gen:
+
+            if "token_likelihoods" in gen:
                 token_likelihoods = []
-                for likelihoods in gen['token_likelihoods']:
-                    token_likelihood = likelihoods['likelihood'] if 'likelihood' in likelihoods else None
-                    token_likelihoods.append(TokenLikelihood(likelihoods['token'], token_likelihood))
-            generations.append(Generation(gen['text'], token_likelihoods))
+                for likelihoods in gen["token_likelihoods"]:
+                    token_likelihood = likelihoods["likelihood"] if "likelihood" in likelihoods else None
+                    token_likelihoods.append(TokenLikelihood(likelihoods["token"], token_likelihood))
+            generations.append(Generation(gen["text"], token_likelihoods))
         return Generations(generations)
 
-    def embed(
-        self,
-        texts: List[str],
-        truncate: str = None,
-        variant: str = None
-    ) -> Embeddings:
-        json_params = {
-            'texts': texts,
-            'truncate': truncate
-        }
+    def embed(self, texts: List[str], truncate: str = None, variant: str = None) -> Embeddings:
+        json_params = {"texts": texts, "truncate": truncate}
         for key, value in list(json_params.items()):
             if value is None:
                 del json_params[key]
         json_body = json.dumps(json_params)
 
         params = {
-            'EndpointName': self._endpoint_name,
-            'ContentType': 'application/json',
-            'Body': json_body,
+            "EndpointName": self._endpoint_name,
+            "ContentType": "application/json",
+            "Body": json_body,
         }
         if variant is not None:
-            params['TargetVariant'] = variant
+            params["TargetVariant"] = variant
 
         try:
             result = self._client.invoke_endpoint(**params)
-            response = json.loads(result['Body'].read().decode())
+            response = json.loads(result["Body"].read().decode())
         except EndpointConnectionError as e:
             raise CohereError(e)
         except Exception as e:
             # TODO should be client error - distinct type from CohereError?
-            # ValidationError, e.g. when variant is bad 
+            # ValidationError, e.g. when variant is bad
             raise CohereError(e)
 
-        return Embeddings(response['embeddings'])
+        return Embeddings(response["embeddings"])
 
-    def rerank(self,
-               query: str,
-               documents: Union[List[str], List[Dict[str, Any]]],
-               top_n: int = None,
-                variant: str = None) -> Reranking:
+    def rerank(
+        self, query: str, documents: Union[List[str], List[Dict[str, Any]]], top_n: int = None, variant: str = None
+    ) -> Reranking:
         """Returns an ordered list of documents oridered by their relevance to the provided query
         Args:
             query (str): The search query
@@ -146,32 +132,28 @@ class Client:
         parsed_docs = []
         for doc in documents:
             if isinstance(doc, str):
-                parsed_docs.append({'text': doc})
-            elif isinstance(doc, dict) and 'text' in doc:
+                parsed_docs.append({"text": doc})
+            elif isinstance(doc, dict) and "text" in doc:
                 parsed_docs.append(doc)
             else:
                 raise CohereError(
-                    message='invalid format for documents, must be a list of strings or dicts with a "text" key')
+                    message='invalid format for documents, must be a list of strings or dicts with a "text" key'
+                )
 
-        json_params = {
-            "query": query,
-            "documents": parsed_docs,
-            "top_n": top_n,
-            "return_documents": False
-        }
+        json_params = {"query": query, "documents": parsed_docs, "top_n": top_n, "return_documents": False}
         json_body = json.dumps(json_params)
 
         params = {
-            'EndpointName': self._endpoint_name,
-            'ContentType': 'application/json',
-            'Body': json_body,
+            "EndpointName": self._endpoint_name,
+            "ContentType": "application/json",
+            "Body": json_body,
         }
         if variant is not None:
-            params['TargetVariant'] = variant
+            params["TargetVariant"] = variant
 
         try:
             result = self._client.invoke_endpoint(**params)
-            response = json.loads(result['Body'].read().decode())
+            response = json.loads(result["Body"].read().decode())
             reranking = Reranking(response)
             for rank in reranking.results:
                 rank.document = parsed_docs[rank.index]
@@ -179,9 +161,9 @@ class Client:
             raise CohereError(e)
         except Exception as e:
             # TODO should be client error - distinct type from CohereError?
-            # ValidationError, e.g. when variant is bad 
+            # ValidationError, e.g. when variant is bad
             raise CohereError(e)
-        
+
         return reranking
 
     def finetune(
@@ -189,9 +171,9 @@ class Client:
         name: str,
         train_data: str,
         val_data: Optional[str] = None,  # TODO: support val_data
-        base_model: str = "english-v1", 
+        base_model: str = "english-v1",
         instance_type: str = "ml.g4dn.xlarge",
-        training_parameters: Dict[str, Any]={},  # Optional, training algorithm specific hyper-parameters
+        training_parameters: Dict[str, Any] = {},  # Optional, training algorithm specific hyper-parameters
     ):
         if self._endpoint_name != "cohere-classification-finetuning":
             raise CohereError(f"co.finetune() is not supported for endpoint {self._endpoint_name}")
@@ -209,23 +191,25 @@ class Client:
             output_path=self._output_model_dir,
             hyperparameters={"name": name},
         )
-        # TODO: train_data should work with s3:// urls too
-        data_location = estimator.sagemaker_session.upload_data(train_data, key_prefix="cohere-finetune-data")  
-        estimator.fit({"training": data_location})
-        output_model_dir_slash = self._output_model_dir + ('/' if not self._output_model_dir.endswith('/') else "")
-        current_filename = output_model_dir_slash + f"{name}/output/model.tar.gz"
-        bucket, old_key = parse_s3_url(current_filename)
 
-        s3_resource = boto3.resource('s3')
+        # TODO: train_data should work with s3:// urls too
+        data_location = estimator.sagemaker_session.upload_data(train_data, key_prefix="cohere-finetune-data")
+        estimator.fit({"training": data_location})
+        job_name = estimator.latest_training_job.name
+
+        output_model_dir_slash = self._output_model_dir + ("/" if not self._output_model_dir.endswith("/") else "")
+        current_filepath = f"{output_model_dir_slash}{job_name}/output/model.tar.gz"
+
+        s3_resource = boto3.resource("s3")
 
         # Copy new model to root of output_model_dir
-        new_path = output_model_dir_slash + f"{name}.tar.gz"
-        _, new_key = parse_s3_url(new_path)
-        s3_resource.Object(bucket, new_key).copy_from(CopySource={'Bucket': bucket, 'Key': old_key})
+        bucket, old_key = parse_s3_url(current_filepath)
+        _, new_key = parse_s3_url(f"{output_model_dir_slash}{name}.tar.gz")
+        s3_resource.Object(bucket, new_key).copy_from(CopySource={"Bucket": bucket, "Key": old_key})
 
         # Delete old dir
-        bucket, old_short_key = parse_s3_url(output_model_dir_slash + name)
-        s3_resource.Bucket(bucket).objects.filter(Prefix=old_short_key + "/").delete()
+        bucket, old_short_key = parse_s3_url(output_model_dir_slash + job_name)
+        s3_resource.Bucket(bucket).objects.filter(Prefix=old_short_key).delete()
 
     def deploy(self, instance_type: str = "ml.g4dn.xlarge", n_instances: int = 1):
         if self._endpoint_name == "cohere-classification-finetuning":
@@ -246,7 +230,9 @@ class Client:
             if getattr(self, "_request_sender", None) is None:
                 self._request_sender = ClassifyRequestSender(self._endpoint_name, self._client)
 
-            model_path = self._output_model_dir + '/' if not self._output_model_dir.endswith('/') else "" + f"{name}.tar.gz"
+            model_path = (
+                self._output_model_dir + "/" if not self._output_model_dir.endswith("/") else "" + f"{name}.tar.gz"
+            )
             return self._request_sender.send_request(input, name, model_path)
         else:
             raise ValueError(f"co.classify() is not supported for endpoint {self._endpoint_name}")
