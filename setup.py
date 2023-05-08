47a6e33193e65ea49a47bd57d09c844bc4feaac8
Lucas Fayoux <lucas@cohere.com>
2023-05-08 17:14:53 -0400

    add new param to rerank endpoint

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class BinaryDistribution(Distribution):
 
 
 setuptools.setup(name='cohere-sagemaker',
-                 version='0.5.0',
+                 version='0.5.1',
                  author='Cohere',
                  author_email='support@cohere.ai',
                  description='A Python library for the Cohere endpoints in AWS Sagemaker',
